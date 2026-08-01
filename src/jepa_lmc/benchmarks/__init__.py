7407146509266887820366288d2e8cc86c851992
cantodea <9232246@qq.com>
2026-08-01 22:13:59 +0200

    Export random GridWorld benchmark

--- src/jepa_lmc/benchmarks/__init__.py
+++ src/jepa_lmc/benchmarks/__init__.py
@@ -2,9 +2,19 @@
 
 from jepa_lmc.benchmarks.ctl_suite import CTLProperty, default_ctl_suite
 from jepa_lmc.benchmarks.perturbations import block_entries_to_proposition
+from jepa_lmc.benchmarks.random_gridworld import (
+    GridWorldBenchmarkSplits,
+    GridWorldSpec,
+    generate_random_gridworld_spec,
+    make_pilot_benchmark_splits,
+)
 
 __all__ = [
     "CTLProperty",
+    "GridWorldBenchmarkSplits",
+    "GridWorldSpec",
     "block_entries_to_proposition",
     "default_ctl_suite",
+    "generate_random_gridworld_spec",
+    "make_pilot_benchmark_splits",
 ]
